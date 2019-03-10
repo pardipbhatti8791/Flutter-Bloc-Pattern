107a4e6e38730ec566391b76e8421c6f7655f081
Pardip Bhatti <pardipbhatti8791@gmail.com>
2019-03-10 23:43:01 +0530

    created separeted bloc file with StreamTransformers

---
+++ lib/src/blocs/validators.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+class Validators {
+  final validateEmail = StreamTransformer<String, String>.fromHandlers(
+    handleData: (email, sink) {
+      bool emailMatched = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
+      if(emailMatched) {
+        sink.add(email);
+        sink.addError("");
+      } else {
+        sink.addError('Please enter a valid email');
+      }
+    }
+  );
+
+  final validatePassword =StreamTransformer<String, String>.fromHandlers(
+    handleData: (password, sink) {
+      if(password.length > 3) {
+        sink.add(password);
+        sink.addError("");
+      } else {
+        sink.addError('Password must be 4 characters');
+      }
+    }
+  );
+}
